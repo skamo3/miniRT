8d25fe2492c59f4c89c04c5b2a9c8900d9317421
Joochan Kim <joockim@c1r5s3.42seoul.kr>
2020-11-01 01:20:27 +0900

    light

--- srcs/ray_tracing.c
+++ srcs/ray_tracing.c
@@ -6,7 +6,7 @@
 /*   By: joockim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 17:13:43 by joockim           #+#    #+#             */
-/*   Updated: 2020/10/31 19:17:47 by joockim          ###   ########.fr       */
+/*   Updated: 2020/11/01 01:19:34 by joockim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,22 +80,63 @@ int			is_light(t_p3 o, t_p3 d, t_fig *lst)
 		if (lst->flag == SP)
 			in = sphere_inter(o, d, lst);
 		else if (lst->flag == PL)
-			in = plane_inter(ray.o, ray.d, lst->fig.pl.p, lst->normal);
+			in = plane_inter(o, d, lst->fig.pl.p, lst->normal);
 		else if (lst->flag == TR)
-			in = triangle_inter(ray.o, ray.d, lst);
+			in = triangle_inter(o, d, lst);
 		else if (lst->flag == SQ)
-			in = square_inter(ray.o, ray.d, lst);
+			in = square_inter(o, d, lst);
 		else if (lst->flag == CY)
-			in = cylinder_inter(ray.o, ray.d, lst);
+			in = cylinder_inter(o, d, lst);
 		else if (lst->flag == CU)
-			in = cube_inter(ray.o, ray.d, lst);
+			in = cube_inter(o, d, lst);
 		else if (lst->flag == PY)
-			in = pyramid_inter(ray.o, ray.d, lst);
+			in = pyramid_inter(o, d, lst);
 		if (in > EPSILON && in < 1)
 			return (0);
 		lst = lst->next;
 	}
 	return (1);
+}
+
+t_p3		reflect_ray(t_p3 ray, t_p3 normal)
+{
+	return (vsubstract(scal_x_vec(2 * vdot(normal, ray), normal), ray));
+}
+
+double		calc_specular(t_v3 ray, t_inter *inter, t_scene data, t_fig *lst)
+{
+	double	light;
+	t_p3	direction;
+	t_p3	p_to_cam;
+	t_p3	reflected;
+
+	direction = vsubstract(data.l->o, inter->p);
+	p_to_cam = vsubstract(ray.o, inter->p);
+	reflected = reflect_ray(direction, inter->normal);
+	if (vdot(reflected, p_to_cam) > 0)
+		light = data.l->br * pow(vcos(reflected, p_to_cam), lst->specular);
+	else
+		light = 0;
+	return (light);
+}
+
+int			color_x_light(int color, double rgb[3])
+{
+	unsigned int	mask;
+	unsigned int	r;
+	unsigned int	g;
+	unsigned int	b;
+
+	mask = 255 << 16;
+	r = rgb[0] * ((color & mask) >> 16);
+	mask >>= 8;
+	g = rgb[1] * ((color & mask) >> 8);
+	mask >>= 8;
+	b = rgb[2] * (color & mask);
+	r = r > 255 ? 255 : r;
+	g = g > 255 ? 255 : g;
+	b = b > 255 ? 255 : b;
+	return ((r << 16) | (g << 8) | b);
 }
 
 void		compute_light(t_v3 ray, t_inter *inter, t_scene data, t_fig *lst)
@@ -110,8 +151,19 @@ void		compute_light(t_v3 ray, t_inter *inter, t_scene data, t_fig *lst)
 	while (data.l)
 	{
 		direction = vsubstract(data.l->o, inter->p);
-		if 
+		if (is_light(inter->p, direction, lst) && vdot(inter->normal, direction) > 0)
+		{
+			light = data.l->br * vcos(inter->normal, direction);
+			add_coefficient(&rgb, light, data.l->color);
+		}
+		if (lst->specular)
+		{
+			light = calc_specular(ray, inter, data, lst);
+			add_coefficient(&rgb, light, data.l->color);
+		}
+		data.l = data.l->next;
 	}
+	inter->color = color_x_light(inter->color, rgb);
 }
 
 int			trace_ray(t_p3 o, t_p3 d, t_wrap *w, int depth)
